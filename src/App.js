509360e7e590315fc99b393539257502520c2957
Logan Bertrand <loganwbertrand@gmail.com>
2019-07-10 12:32:38 -0700

    components created, working through render troubles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Navbar from './components/Navbar'
+import Jumbotron from './components/Jumbotron'
+import Container from './components/Container'
+import Card from './components/Card'
+import characters from "./characters.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  // Setting this.state.characters to the characters json array
+  state = {
+    characters
+  };
+
+  render(){
+    return (
+      <div>
+        <Navbar/>
+        <Jumbotron/>
+        <Container>
+        {this.state.characters.map(character => (
+          <Card
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        ))}
+        </Container>
+        
+      </div>
+    );
+  }
+};
+
 
 export default App;
