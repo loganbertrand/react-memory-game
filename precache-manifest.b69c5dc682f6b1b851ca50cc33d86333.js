3b7840e12d796256b331647cde833bf436a832b3
Logan Bertrand <loganwbertrand@gmail.com>
2019-07-12 12:57:15 -0700

    Updates

---
+++ precache-manifest.b69c5dc682f6b1b851ca50cc33d86333.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e81c0abce32064f565e78722412c6ce",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "b81d0e7128e26a88c2cb",
+    "url": "/react-memory-game/static/css/main.4e27dfe3.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-memory-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b81d0e7128e26a88c2cb",
+    "url": "/react-memory-game/static/js/main.96528198.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  }
+]);
